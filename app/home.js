5804cff96c90095b03eaee3b1d7a59d2d477f6f8
sexyoung <Habuche@gmail.com>
2017-04-21 20:48:30 +0800

    加一些修改

--- app/home.js
+++ app/home.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import style from './home.scss';
-import img from './cat.jpg';
+// import img from './cat.jpg';
 
 // function component () {
 //   var element = document.createElement('div');
@@ -11,4 +11,14 @@ import img from './cat.jpg';
 // }
 //
 // document.body.appendChild(component());
-console.warn('hello', style, img);
+// console.warn('hello', style, img);
+
+
+$('input:radio').on('click', function(e) {
+  // console.info(e);
+  console.info($(e.target).val());
+});
+
+
+// console.warn($('#a1').prop('checked')); // DOM
+// console.warn($('#a1').attr('checked')); // HTML
